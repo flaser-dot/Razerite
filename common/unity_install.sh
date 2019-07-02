b1536643148c0d8e3d13f1185c0fcb647c6c3688
LordVicky <sreevicky1001@gmail.com>
2019-07-02 15:36:13 +0400

    Update unity_install.sh

--- common/unity_install.sh
+++ common/unity_install.sh
@@ -68,9 +68,9 @@ fi
 ui_print " "
 ui_print " - Media Option -"
 ui_print " *******************************************"
-ui_print "   Do you want Razer custom media sounds and wallpapers?"
+ui_print "   Do you want to install Razer custom media sounds?"
 ui_print "   They include ringtones, alarms, notifications,"
-ui_print "   UI sounds and wallpapers extracted from RazerPhone"
+ui_print "   And UI sounds ported from RazerPhone2"
 ui_print "   Vol Up = Yes, Vol Down = No"
 if $VKSEL; then
   ui_print " "
@@ -83,6 +83,63 @@ else
 fi
 
 
+if [ "$API" -ge 27 ]; then
+  ui_print " "
+  ui_print " *******************************************"
+  ui_print "   Do you want to install Razer Wallpapers and Nova Launcher?"
+  ui_print "   Vol Up = Yes, Vol Down = No"
+  if $VKSEL; then
+    ui_print " "
+    ui_print "   Enabling Razer Wallpapers and Nova Launcher..."
+  else
+    ui_print " "
+    ui_print "  Disabling Razer Wallpapers and Nova Launcher..."
+    rm -rf "$TMPDIR"/system/app/RazerWallpapers
+    rm -rf "$TMPDIR"/system/app/NovaLauncherOverlay
+    rm -rf "$TMPDIR"/system/priv-app/NovaLauncher
+  fi
+
+else
+    rm -rf "$TMPDIR"/system/app/RazerWallpapers
+    rm -rf "$TMPDIR"/system/app/NovaLauncherOverlay
+    rm -rf "$TMPDIR"/system/priv-app/NovaLauncher
+fi
+
+
+
+if [ "$API" -ge 27 ]; then
+  ui_print " "
+  ui_print " *******************************************"
+  ui_print "   Do you want to install Razer Cortex?"
+  ui_print "   Vol Up = Yes, Vol Down = No"
+  if $VKSEL; then
+    ui_print " "
+    ui_print "   Enabling Razer Cortex..."
+  else
+    ui_print " "
+    ui_print "  Disabling Razer Cortex..."
+    rm -rf "$TMPDIR"/system/priv-app/RazerCortex
+  fi
+else
+    rm -rf "$TMPDIR"/system/priv-app/RazerCortex
+fi
+
+if [ "$API" -ge 27 ]; then
+  ui_print " "
+  ui_print " *******************************************"
+  ui_print "   Do you want to install Razer Game Booster (Beta)?"
+  ui_print "   Vol Up = Yes, Vol Down = No"
+  if $VKSEL; then
+    ui_print " "
+    ui_print "   Enabling Razer Game Booster..."
+  else
+    ui_print " "
+    ui_print "  Disabling Razer Game Booster..."
+    rm -rf "$TMPDIR"/system/priv-app/RazerGameBooster
+  fi
+else
+    rm -rf "$TMPDIR"/system/priv-app/RazerGameBooster
+fi
 
 ui_print "
 
